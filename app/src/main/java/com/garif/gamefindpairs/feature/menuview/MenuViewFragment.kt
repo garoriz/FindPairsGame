27c3e875b160d5063488a8fad21fe257a6855c45
rizvan <rizvangaripov@gmail.com>
2023-12-18 18:48:28 +0300

    init commit, add menu view, coins view, game logo

---
+++ app/src/main/java/com/garif/gamefindpairs/feature/menuview/MenuViewFragment.kt
@@ -0,0 +1,12 @@
+package com.garif.gamefindpairs.feature.menuview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.garif.gamefindpairs.R
+
+class MenuViewFragment : Fragment(R.layout.fragment_menu_view) {
+
+}
